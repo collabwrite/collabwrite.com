4f40a9c7ff164b2de6d7649202ab813a4e30c8f9
Umesh <umesh.ganapathy@gmail.com>
2024-07-28 07:38:25 -0700

    add mdx render and basic blog generator with md content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,36 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
+import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
+import remarkUnwrapImages from 'remark-unwrap-images'
+import rehypeExternalLinks from 'rehype-external-links'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.collabwrite.com',
-  integrations: [tailwind(), sitemap()]
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }),
+    sitemap(),
+    mdx()
+  ],
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          rel: ['nofollow, noopener, noreferrer']
+        }
+      ]
+    ],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: ['']
+      }
+    }
+  }
 })
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-    './node_modules/flowbite/**/*.js'
-  ],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     screens: {
       sm: '480px',
@@ -36,7 +33,7 @@ export default {
       fontFamily: {
         SpaceGrotesk: ['Space Grotesk', 'sans-serif']
       }
-    },
-    plugins: [require('flowbite/plugin')]
-  }
+    }
+  },
+  plugins: [require('@tailwindcss/typography')]
 }
